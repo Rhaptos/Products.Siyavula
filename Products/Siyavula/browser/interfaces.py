c103e2daf4292f26be6de8c2bfb93ec1c31f97ef
Ross J. Reedstrom <reedstrm@cnx.org>
2011-06-16 03:52:57 +0000

    inteface import fixup

--- Products/Siyavula/browser/interfaces.py
+++ Products/Siyavula/browser/interfaces.py
@@ -1,5 +1,5 @@
 from zope.interface import Interface
-from zope.app.publisher.interfaces.browser import IBrowserView
+from zope.publisher.interfaces.browser import IBrowserView
 
 class ISiyavulaView(IBrowserView):
     """ 
